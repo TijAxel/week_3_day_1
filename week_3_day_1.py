abb673f9a1e26b145a2b56c781d0d99f06710232
Salazar <asalazar58@cps.k12.il.us>
2024-09-11 12:03:03 -0500

    Valid Variables

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -47,36 +47,36 @@
 
 
 
-# # Review
-# create variables for the following :
-# 1. age
-age =int(input("How old are you?: "))
-# 2. name
-name = input("what is your name?: ")
-# 3. song
-song= input("What is your favorite song?:")
-# 4. food
-food = input("Waht is your favorite food?: ")
-# 5. number
-number = int(input("what is your favorite number?: "))
+# # # Review
+# # create variables for the following :
+# # 1. age
+# age =int(input("How old are you?: "))
+# # 2. name
+# name = input("what is your name?: ")
+# # 3. song
+# song= input("What is your favorite song?:")
+# # 4. food
+# food = input("Waht is your favorite food?: ")
+# # 5. number
+# number = int(input("what is your favorite number?: "))
 
 
-# #now include the variables you just made print in the following...
+# # #now include the variables you just made print in the following...
 
 
-# Once upon a time, there was a [age] old coder named [name].
+# # Once upon a time, there was a [age] old coder named [name].
 
 
-# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+# # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
 
 
-# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
-##########################################################################################
+# # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+# ##########################################################################################
 
 
-print(f"Once upon a time, there was a {age} old coder named {name} ")
-print(f"{name} liked to hum the song {song} while coding. It was so annoying that their teammates would throw {food} "until" ,name, "would stop singing. ")
-print ("Still," ,name, "was the best coder on the team and could write" ,number, "lines of code every day. Maybe" ,song, "was" ,name,"'s secret power?" )
+# print(f"Once upon a time, there was a {age} old coder named {name} ")
+# print(f"{name} liked to hum the song {song} while coding. It was so annoying that their teammates would throw {food} until {name} would stop singing. ")
+# print (f"Still, {name} was the best coder on the team and could write {number} lines of code every day. Maybe {song} was {name}'s secret power?" )
 
 
 
@@ -107,13 +107,13 @@ print ("Still," ,name, "was the best coder on the team and could write" ,number,
 # Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
 
 
-# 1st_name
-# last name
-# email@address
-# percent%
-# variable#name
-# O
-# list
+# first_name
+# last_name
+# email_address
+# percent
+# variable_name
+# Zweo
+# list_of_names
 # Creating Valid Names: Create valid names for the following descriptions:
 
 
@@ -134,9 +134,9 @@ print ("Still," ,name, "was the best coder on the team and could write" ,number,
 # email_address
 # percentage
 # variable_name
-# 1_variable
-# email@address
-# percentage%
+# one_variable
+# email_address
+# percentage
 # i
 
 
@@ -172,13 +172,21 @@ print ("Still," ,name, "was the best coder on the team and could write" ,number,
 
 ##########################################################################################
 # #addition
+print(2+2)
 # #multiplication
+print(2*2)
 # #division
+print(2/2)
 # #modulo
+
 # #powers
+
 # #get the max and min of a number
+
 # #round a number
+
 # # absolute value
+
 # # order of operations
 # #to do more you need to import special math libraries from python
 # #from math import *    
